856e66aebec5c256e45aecb3af0fbbb95e0ab48c
stmarkjonathan <98546985+stmarkjonathan@users.noreply.github.com>
2024-03-21 10:45:38 -0400

    adding interactable grid

---
+++ grid.js
@@ -0,0 +1,62 @@
+const getHeader = function(){
+    return document.getElementById("header");
+}
+
+const getGrid = function(){
+    return document.getElementById("grid");
+}
+
+const recipeJsonObject = '{}';
+
+const json = function(){
+    fetch('recipe.json')
+    .then(response => response.json())
+    .then(jsonResponse => console.log(jsonResponse));
+    recipeJsonObject = jsonResponse.p
+    
+}
+
+const recipes = new Array(12);
+
+
+var headerContent;
+var gridContent;
+
+function dropdownItem(id){
+    document.getElementById(id).classList.toggle("show");
+}
+
+function selectDropdownItem(item){
+    document.getElementById("footer").innerHTML = document.getElementById(item).innerHTML;
+}
+
+function createRecipes(){
+    for(let i = 0; i < recipes.length ; i++){
+        getGrid().innerHTML += 
+        `<button onclick="viewRecipe('recipe')" class="recipe-item-button" type="button">
+              <div class="recipe-item">
+  
+                Recipe` +i+ `
+                
+              </div>
+        </button>`
+    }
+    
+    json();
+}
+
+/*function viewRecipe(recipe){
+
+    headerContent = getHeader().innerHTML;
+    gridContent = getGrid().innerHTML;
+
+    getHeader().innerHTML = "<button onclick='returnToSelection()'>Go back</button>";
+    getGrid().innerHTML = "Recipe 1";
+}*/
+
+function returnToSelection(){
+
+    getHeader().innerHTML = headerContent;
+    getGrid().innerHTML = gridContent;
+
+}
