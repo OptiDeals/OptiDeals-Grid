45e740a2d999fd90621e39fc2714ef9d95d3b275
stmarkjonathan <98546985+stmarkjonathan@users.noreply.github.com>
2024-10-12 17:51:57 -0400

    updating website

--- index.js
+++ index.js
@@ -1,190 +1,221 @@
-var currentRecipeJson;
-var filteredJson;
+var currentConstraint = 0;
+var currentStore = "all";
 
+const sqlPromise = initSqlJs({
+    locateFile: file => `sql-wasm.wasm`
+});
 
+async function fetchDatabase(sqlPromise) {
+    const dataPromise = fetch("https://raw.githubusercontent.com/OptiDeals/OptiDeals-Data/refs/heads/main/data/optideals.db").then(res => res.arrayBuffer());
+    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+    return new SQL.Database(new Uint8Array(buf));
+}
 
+async function loadRecipes() {
+    const db = await fetchDatabase(sqlPromise);
+    const stmt = db.prepare("SELECT recipe_title, recipe_image FROM recipes");
+
+    while (stmt.step()) {
+        const row = stmt.getAsObject();
+
+    }
+}
 window.addEventListener('load', Init)
 
-
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 
 
 function myFunction() {
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
 
-if (window.scrollY > sticky) {
-header.classList.add("sticky");
-} else {
-header.classList.remove("sticky");
+    if (window.scrollY > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
-}
-
-const links =
-[
-    "metroRecipes.json",
-    "foodbasicsRecipes.json"
-]
 
 //get header html container
-const getHeader = function(){
+const getHeader = function () {
     return document.getElementById("header");
 }
 
 //get grid html container
-const getGrid = function(){
+const getGrid = function () {
     return document.getElementById("grid");
 }
 
-//fetch json
-function getJson(link){
-    fetch(link)
-    .then(response => response.json())
-    .then(data => saveJson(data));
-}
-
-
-//start adding recipe cards to page
-function saveJson(obj){
-    currentRecipeJson = obj;
-    displayJson(currentRecipeJson);
-}
-
-function displayJson(obj){
+async function displayRecipes() {
 
     getGrid().innerHTML = "";
-    
-    for(let i = 0; i < obj.length ; i++){
-        if(obj[i] != null){
-        getGrid().innerHTML += 
-            `<button onclick="viewRecipe('recipe')" class="recipe-item-button" type="button">
-                  <div class="recipe-item">
-                    `+obj[i].name+`
-                    <br><br>
-                    <span>`+obj[i].description+`</span>
-                    <br><br>
-                    <span>Serves `+obj[i].serves+`<span>
-                    <br>
-                    <ul>
-                    `+populateCard(obj[i].ingredients)+`
-                    </ul> 
-                    <span>Total Cost: $`+obj[i].total_cost+`</span>     
-                  </div>
-            </button>`
-        }      
-    }
+
+    const db = await fetchDatabase(sqlPromise);
+    const recipes_stmt = db.prepare("SELECT * FROM recipes");
+
+    //run through each recipe in the recipes table
+    while (recipes_stmt.step()) {
+        const row = recipes_stmt.getAsObject();
+        const ingredients_stmt = db.prepare("SELECT * FROM recipe_ingredients WHERE recipe_id = " + row.id);
+
+        if(currentStore == "all" || row.recipe_store == currentStore)
+        {
+            if(currentConstraint == 0 || row.recipe_total_cost < currentConstraint)
+                {
+                let ingredientsHTML = "";
+        
+                //run through each ingredient of current recipe
+                while (ingredients_stmt.step()) {
+        
+                    const ingredients_row = ingredients_stmt.getAsObject();
+                    ingredientsHTML += 
+                    `<li>${ingredients_row.recipe_ingredient}, 
+                        ${ingredients_row.recipe_ingredient_amount}, 
+                        $${ingredients_row.recipe_ingredient_cost}</li>`
+                }
+        
+                const blob = new Blob([row.recipe_image], { type: 'image/jpeg' });
+                recipe_img = URL.createObjectURL(blob);
+        
+                getGrid().innerHTML +=
+                    `<button onclick="viewRecipe('recipe')" class="recipe-item-button" type="button">
+                          <div class="recipe-item">
+                            <div class="recipe-info">
+                            `+ row.recipe_title + row.recipe_store +`
+                            <br><br>
+                            <span>`+ row.recipe_description + `</span>
+                            <br><br>
+                            <span>Serves `+ row.recipe_serving_size + `<span>
+                            <br>
+                            <div class = "recipe-list">
+                                <ul>
+                                `+ingredientsHTML+`
+                                </ul> 
+                            </div>
+                            <br>
+                            <span>Total Cost: $`+ Math.round(row.recipe_total_cost * 100) / 100 + `</span>     
+                            </div>
+                            <div class="recipe-img">
+                            <img src="`+recipe_img+`" width="550" height ="500">
+                            </div>
+                          </div>
+                    </button>`
+            }else{
+                continue
+            }
+        }
+
+        
+}
 
 
 }
- 
+
 
 //populate each card with recipe
-function populateCard(ingredients){
+function formatIngredients(name, amount, cost) {
     let html = ``;
-    
-    for(let i = 0; i < ingredients.length ; i++){
 
-        html += `<li>${ingredients[i].name}, 
-                ${ingredients[i].amount}, 
-                $${ingredients[i].cost}</li>`
-    }
+    html += `<li>${name}, 
+                ${amount}, 
+                $${cost}</li>`
+
 
     return html;
 }
 
-function filterJsonData(constraint){
-
-    filteredJson = JSON.parse(JSON.stringify(currentRecipeJson));
-
-    //filter json
-    for(let i = 0; i < filteredJson.length ; i++){
-
-        if(filteredJson[i].total_cost > constraint){
-
-            filteredJson[i] = null;
-
-        }
-
-    }
-
-    //check if all json data is null
-    if(filteredJson.every(e => e === null)){
-        getGrid().innerHTML = "Could not find any recipes with selected filters.";
-    }else{
-        displayJson(filteredJson);      
-    }
-
-
+function setConstraint(constraint) {
+    currentConstraint = constraint;
 }
 
-function highlightButton(id){
+function setStore(store) {
+    currentStore = store;
+}
+
+function highlightButton(id) {
 
     var clickedElement = document.getElementById(id);
-   
+
 
     document.querySelectorAll("[class='highlight-button']").forEach
-    (
-        item => {
-            
-            //any button with the same parent as the clicked dropdown button will have it's checkmark removed
-            //so that only one checkmark per dropdown is active
-            var dropdownParent = item.parentElement.parentElement;
-            
-            if(dropdownParent == clickedElement.parentNode){
+        (
+            item => {
 
-                item.classList.add("hide");
-        
+                //any button with the same parent as the clicked dropdown button will have it's checkmark removed
+                //so that only one checkmark per dropdown is active
+                var dropdownParent = item.parentElement.parentElement;
+
+                if (dropdownParent == clickedElement.parentNode) {
+
+                    item.classList.add("hide");
+
+                }
             }
-        }
-          
-    );
+
+        );
 
     document.querySelectorAll("[id=" + CSS.escape(id) + "] > *").forEach(item => item.classList.remove("hide"));
 }
 
 
 
-function dropdownItem(id){
+function dropdownItem(id) {
 
-    if(document.getElementById(id).classList.contains("show")){
+    if (document.getElementById(id).classList.contains("show")) {
         document.getElementById(id).classList.remove("show");
-    }else{
+    } else {
         document.getElementById(id).classList.add("show");
     }
 
-    
+
 }
 
-function Init(){
+function Init() {
 
-    getJson(links[0]); 
+    loadRecipes();
+    displayRecipes();
 
     document.getElementById('storeItem1').addEventListener('click', function () {
-        getJson(links[0]);
+        setStore("metro")
         highlightButton('storeItem1');
+        displayRecipes();
     })
 
     document.getElementById('storeItem2').addEventListener('click', function () {
-        getJson(links[1]);
+        setStore("foodbasics")
         highlightButton('storeItem2');
+        displayRecipes();
+    })
+
+    document.getElementById('storeItem3').addEventListener('click', function () {
+        setStore("all")
+        highlightButton('storeItem3');
+        displayRecipes();
     })
 
     document.getElementById('priceUnder20').addEventListener('click', function () {
-        filterJsonData(20);
+        setConstraint(20);
         highlightButton('priceUnder20');
+        displayRecipes()
     })
 
     document.getElementById('priceUnder30').addEventListener('click', function () {
-        filterJsonData(30);
+        setConstraint(30);
         highlightButton('priceUnder30');
+        displayRecipes()
     })
 
     document.getElementById('priceUnder40').addEventListener('click', function () {
-        filterJsonData(40);
+        setConstraint(40);
         highlightButton('priceUnder40');
+        displayRecipes()
     })
 
-    
+    document.getElementById('priceNoFilter').addEventListener('click', function () {
+        setConstraint(0);
+        highlightButton('priceNoFilter');
+        displayRecipes();
+    })
 }
